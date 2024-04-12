233f834421f41ecdca2054beb42d152134bfdd7a
chaitanyabatra <71209117+chaitanyabatra@users.noreply.github.com>
2024-04-12 09:05:33 +0530

    Time: 161 ms (5.05%) | Memory: 13.6 MB (9.52%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        n=len(height)
+        prefix=[0]*n
+        suffix=[0]*n
+        maxprefix=0
+        for i in range(n):
+            prefix[i]=max(maxprefix,height[i])
+            maxprefix=prefix[i]
+        maxsuffix=0
+        print(prefix)
+        for i in range(n-1,-1,-1):
+            suffix[i]=max(maxsuffix,height[i])
+            maxsuffix=suffix[i]
+        print(suffix)
+        water=[0]*n
+        for i in range(n):
+            water[i]=min(prefix[i],suffix[i])
+            water[i]-=height[i]
+        print(water)
+        return sum(water)
+
+        
