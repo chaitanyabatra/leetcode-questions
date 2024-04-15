cb361fa3e9f9464b4f55decf7cf4d715c95efc7e
chaitanyabatra <71209117+chaitanyabatra@users.noreply.github.com>
2024-04-16 01:59:40 +0530

    Time: 39 ms (79.03%) | Memory: 17.3 MB (71.03%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
@@ -1,36 +1,29 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def widthOfBinaryTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        if not root:return 0
-        maxw=1
-        q=deque([root])
-        iq=deque([1])
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        q=deque()
+        iq=deque()
+        q.append(root)
+        iq.append(0)
+        w=1
         while q:
-            levelsize=len(q)
-            l,r=0,0
-
-            for i in range(levelsize):
+            l = len(q)
+            
+            for i in range(l):
                 node=q.popleft()
                 index=iq.popleft()
+                if i==0:b=index
+                if i==l-1:a=index
 
-                if i==0:
-                    l=index
-                if i==levelsize-1:
-                    r=index
-                if node.left:
-                    q.append(node.left)
-                    iq.append(index*2)
-                if node.right:
-                    q.append(node.right)
-                    iq.append(2*index+1)
-            maxw=max(maxw,r-l+1)
-        return maxw
+                if node.left:q.append(node.left);iq.append(index*2)
+                if node.right:q.append(node.right);iq.append(index*2+1)
+            w=max(w,a-b+1)
+        return w
+
