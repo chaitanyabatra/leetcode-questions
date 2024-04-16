bf6aa9ca805b07691999c7e33c95cd8019aaae2f
chaitanyabatra <71209117+chaitanyabatra@users.noreply.github.com>
2024-04-16 17:32:17 +0530

    Time: 60 ms (77.36%) | Memory: 20.9 MB (47.18%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -11,8 +11,10 @@ class Solution:
             nonlocal ms
             if not node:
                 return 0
+                
             l=max(go(node.left),0)
             r=max(go(node.right),0)
+
             ms=max(ms,l+r+node.val)
             return max(l,r)+node.val
         go(root)
