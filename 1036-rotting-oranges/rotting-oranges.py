c8d5e1663bf2ef0c04dc7eaafea06b67b835e63c
chaitanyabatra <71209117+chaitanyabatra@users.noreply.github.com>
2024-06-30 20:24:22 +0530

    Time: 44 ms (88.76%) | Memory: 16.5 MB (81.81%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,11 +1,16 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        '''
+        count total number of oranges, add all rotten x,y in a queue
+        made dx dy as -1 1 0 0
+        did bfs on queue, for each 2, turnt their neighbouring 1s to 2 and added to queue if q still there add 1 to day
+        '''
         m=len(grid)
         n=len(grid[0])
-        total=0
+        total=0#total rotten
         days=0
-        count=0
+        count=0#count rotten
         q=deque()
         for i in range(m):
             for j in range(n):
@@ -13,6 +18,7 @@ class Solution:
                 if grid[i][j]==2:q.append([i,j])
         dx=[0,0,1,-1]
         dy=[1,-1,0,0]
+        if not q and not total:return 0
         while q:
             k=len(q)
             count+=k
@@ -27,8 +33,8 @@ class Solution:
                     if xx>=0 and yy>=0 and xx<m and yy<n and grid[xx][yy]==1:
                         q.append([xx,yy])
                         grid[xx][yy]=2
-            if q:days+=1
-        if total==count:return days
+            days+=1
+        if total==count:return days-1
         return -1
 
 
