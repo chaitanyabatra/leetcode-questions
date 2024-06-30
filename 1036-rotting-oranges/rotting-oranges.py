497df8b3e15b3d9c07bc8a2a5be93f8ce0c2e8cb
chaitanyabatra <71209117+chaitanyabatra@users.noreply.github.com>
2024-06-30 16:42:32 +0530

    Time: 55 ms (28.90%) | Memory: 16.4 MB (81.81%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -0,0 +1,35 @@
+from collections import deque
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        m=len(grid)
+        n=len(grid[0])
+        total=0
+        days=0
+        count=0
+        q=deque()
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]!=0:total+=1
+                if grid[i][j]==2:q.append([i,j])
+        dx=[0,0,1,-1]
+        dy=[1,-1,0,0]
+        while q:
+            k=len(q)
+            count+=k
+            while k:
+                k-=1
+                node=q.popleft()
+                x=node[0]
+                y=node[1]
+                for i in range(4):
+                    xx=x+dx[i]
+                    yy=y+dy[i]
+                    if xx>=0 and yy>=0 and xx<m and yy<n and grid[xx][yy]==1:
+                        q.append([xx,yy])
+                        grid[xx][yy]=2
+            if q:days+=1
+        if total==count:return days
+        return -1
+
+
+        
