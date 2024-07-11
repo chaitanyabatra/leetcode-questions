a55308934a177084052ed1b7bfb46e6a1e408f17
chaitanyabatra <71209117+chaitanyabatra@users.noreply.github.com>
2024-07-11 14:07:16 +0530

    Time: 51 ms (99.56%) | Memory: 16.8 MB (63.62%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,29 +1,14 @@
 class Solution:
     def lengthOfLIS(self, arr: List[int]) -> int:
-        n = len(arr)
-        dp = [1] * n
-        hash_map = list(range(n))
-
-        for i in range(n):
-            for prev_index in range(i):
-                if arr[prev_index] < arr[i] and 1 + dp[prev_index] > dp[i]:
-                    dp[i] = 1 + dp[prev_index]
-                    hash_map[i] = prev_index
-
-        ans = -1
-        last_index = -1
-
-        for i in range(n):
-            if dp[i] > ans:
-                ans = dp[i]
-                last_index = i
-        return ans
-        
-
-        temp = [arr[last_index]]
-
-        while hash_map[last_index] != last_index:
-            last_index = hash_map[last_index]
-            temp.append(arr[last_index])
-
-        temp.reverse()
+        # code here
+        n=len(arr)
+        temp=[arr[0]]
+        l=1
+        for i in range(1,n):
+            if arr[i]>temp[-1]:
+                temp.append(arr[i])
+                l+=1
+            else:
+                ind=bisect.bisect_left(temp,arr[i])
+                temp[ind]=arr[i]
+        return l
