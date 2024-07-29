920613e84d33bab3980460877d46d610b3913030
chaitanyabatra <71209117+chaitanyabatra@users.noreply.github.com>
2024-07-29 21:30:41 +0530

    Time: 29 ms (74.39%) | Memory: 11.8 MB (40.04%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.cpp
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -3,13 +3,6 @@
 
 using namespace std;
 
-static int fastIO = []{
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    return 0;
-}();
-
 class Solution {
 public:
     int numTeams(vector<int>& rating) {
