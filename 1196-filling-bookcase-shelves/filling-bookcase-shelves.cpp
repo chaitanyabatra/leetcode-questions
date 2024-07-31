adcee25128242a3857cea0b55cc8c76e0b87fc3c
chaitanyabatra <71209117+chaitanyabatra@users.noreply.github.com>
2024-07-31 10:49:36 +0530

    Time: 3 ms (87.31%) | Memory: 11.8 MB (43.73%) - LeetSync

---
+++ 1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+        int n=books.size();
+        vector<int> cache(n,-1);
+        function<int(int)> go = [&](int i) {
+            if (i==n) return 0;
+            if (cache[i]!=-1) return cache[i];
+            int currwidth=shelfWidth;
+            int maxheight=0;
+            int minheight=10000000;
+            for(int j=i;j<n;j++){
+                int width=books[j][0];
+                int height=books[j][1];
+                if(width>currwidth) break;
+                currwidth=currwidth-width;
+                maxheight=max(maxheight,height);
+                minheight=min(minheight,go(j+1)+maxheight);
+            }
+            cache[i]=minheight;
+            return cache[i];
+        };
+        return go(0);
+    }
+};
