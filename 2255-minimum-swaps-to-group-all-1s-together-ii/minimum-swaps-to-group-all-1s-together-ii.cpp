f218ab0706c9f97927222d733bc5448e1e2b2093
chaitanyabatra <71209117+chaitanyabatra@users.noreply.github.com>
2024-08-02 12:10:37 +0530

    Time: 44 ms (99.61%) | Memory: 83.1 MB (47.74%) - LeetSync

---
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        int n=nums.size();
+        int total=accumulate(nums.begin(), nums.end(), 0);
+        int ans=total;
+        int temp=accumulate(nums.begin(),nums.begin()+total,0);
+        for (int i=0;i<(n);i++){
+            ans=min(ans,total-temp);
+            temp=temp+nums[(i+total)%n]-nums[i];
+
+        }
+        
+        return ans;
+    }
+};
