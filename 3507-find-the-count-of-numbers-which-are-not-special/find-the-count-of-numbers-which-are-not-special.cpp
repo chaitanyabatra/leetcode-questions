abe4b45dd140928d161bb0825560f4ff4d99abc0
chaitanyabatra <71209117+chaitanyabatra@users.noreply.github.com>
2024-08-02 12:31:19 +0530

    Time: 18 ms (94.75%) | Memory: 26.8 MB (16.87%) - LeetSync

---
+++ 3507-find-the-count-of-numbers-which-are-not-special/find-the-count-of-numbers-which-are-not-special.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int nonSpecialCount(int l, int r) {
+        int total=r-l+1;
+        int n=ceil(sqrt(r))+1;
+        vector<int> sieve(n,true);
+        sieve[0]=false;
+        sieve[1]=false;
+        for(int p=2;p*p<n;p++){
+            if (sieve[p]==true){
+                for (int i=p*p;i<n;i+=p) sieve[i]=false;
+            }
+        }
+        for (int i=ceil(sqrt(l));i<=floor(sqrt(r));i++){
+            if (sieve[i]==true) total--;
+        }
+        return total;
+    }
+};//find primes in range l**0.5 to r**0.5[sieve] and then count how many primes,- that amount from r-l+1
